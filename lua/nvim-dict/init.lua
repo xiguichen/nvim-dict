77107c4e53a0b6f1b7c9cbf20867169e82dac70a
Xigui Chen <xigui.chen@citrix.com>
2024-12-02 22:12:40 +0800

    try to support chinese meaning

--- lua/nvim-dict/init.lua
+++ lua/nvim-dict/init.lua
@@ -7,22 +7,22 @@ local function fetch_english_meaning(word)
   local result = handle:read("*a")
   handle:close()
   local data = vim.fn.json_decode(result)
-  if data and data[1] and data[1].meanings then
+  if data and data[1] and data[1].meanings and data[1].meanings[1] and data[1].meanings[1].definitions and data[1].meanings[1].definitions[1] then
     return data[1].meanings[1].definitions[1].definition
   else
     return "No definition found"
   end
 end
 
--- Function to fetch word meaning from a Chinese dictionary API
+-- Function to fetch word meaning from the Glosbe API
 local function fetch_chinese_meaning(word)
-  local url = "https://api.dictionaryapi.dev/api/v2/entries/zh/" .. word
+  local url = "https://glosbe.com/gapi/translate?from=eng&dest=zho&format=json&phrase=" .. word .. "&pretty=true"
   local handle = io.popen("curl -s " .. url)
   local result = handle:read("*a")
   handle:close()
   local data = vim.fn.json_decode(result)
-  if data and data[1] and data[1].meanings then
-    return data[1].meanings[1].definitions[1].definition
+  if data and data.tuc and data.tuc[1] and data.tuc[1].meanings and data.tuc[1].meanings[1] then
+    return data.tuc[1].meanings[1].text
   else
     return "未找到定义"
   end
